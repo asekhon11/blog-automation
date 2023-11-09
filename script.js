02dad1ffc9820bd0093dcb5fbf2aa29071e05d4b
asekhon11 <101844469+asekhon11@users.noreply.github.com>
2023-11-09 15:47:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,13 @@
-document.getElementById('blog-form').addEventListener('submit', function(e) {
+document.getElementById('blogForm').addEventListener('submit', function(e) {
     e.preventDefault();
-    var blogContent = document.getElementById('blog-content').value;
-
-    fetch('YOUR_BACKEND_ENDPOINT', {  // Replace with your actual backend endpoint
+    const blogContent = this.blogContent.value;
+    fetch('http://localhost:5000/submit-blog', {
         method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ content: blogContent }),
-    })
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById('response-message').innerText = 'Blog submitted successfully!';
-        document.getElementById('blog-content').value = '';  // Clear the text area
-    })
-    .catch((error) => {
-        console.error('Error:', error);
-    });
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({ content: blogContent })
+    }).then(response => response.json())
+      .then(data => {
+          // Handle response here
+          alert('Blog submitted!');
+      });
 });

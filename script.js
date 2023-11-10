92d577e0af0ea39c41a91950971402abf188bc25
asekhon11 <101844469+asekhon11@users.noreply.github.com>
2023-11-09 19:07:04 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 document.getElementById('blogForm').addEventListener('submit', function(e) {
     e.preventDefault();
     const blogContent = this.blogContent.value;
-    fetch('https://8654-99-246-144-174.ngrok-free.app', {
+    fetch('https://8654-99-246-144-174.ngrok-free.app/submit-blog', { // Ensure the URL is correct
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({ content: blogContent })
-    }).then(response => response.json())
-      .then(data => {
-          // Handle response here
-          alert('Blog submitted!');
-      });
+    }).then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        // Handle response here
+        alert('Blog submitted!');
+    })
+    .catch(error => {
+        // Handle errors here
+        console.error('There has been a problem with your fetch operation:', error);
+        alert('Error submitting blog!');
+    });
 });

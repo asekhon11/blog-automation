9ae8820eb7d584e7cdc426d2693644e6508f8172
asekhon11 <101844469+asekhon11@users.noreply.github.com>
2023-11-09 22:02:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.getElementById('blogForm').addEventListener('submit', function(e) {
 
     fetch('https://11ae-99-246-144-174.ngrok-free.app/submit-blog', {
         method: 'POST',
-        headers: {'Content-Type': 'application/json'},
+        headers: {'content': 'application/json'},
         body: JSON.stringify({ content: blogContent })
     })
     .then(response => response.json())

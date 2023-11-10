13e4062b2672cf39c6d59a0ada61df130cd32088
asekhon11 <101844469+asekhon11@users.noreply.github.com>
2023-11-09 19:22:56 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
-document.getElementById('blogForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    const blogContent = this.blogContent.value;
-    fetch('https://11ae-99-246-144-174.ngrok-free.app/submit-blog', {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({ content: blogContent })
-    }).then(response => response.json())
-      .then(data => {
-          // Handle response here
-          alert('Blog submitted!');
-      }).catch(error => {
-          // Handle any errors here
-          console.error('Error:', error);
-          alert('Failed to submit the blog');
-      });
+fetch('https://11ae-99-246-144-174.ngrok-free.app/submit-blog', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({ content: blogContent })
+})
+.then(response => response.json())
+.then(data => {
+    alert('Blog submitted!');
+})
+.catch(error => {
+    console.error('Error:', error);
 });
